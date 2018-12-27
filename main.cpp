41bc42d5d4b1421ad2d5b41c76c5bb909e76664c
s191023 <sjw171.sw@gmail.com>
2018-12-26 19:28:52 -0500

    Add files via upload
    My first project ever....

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Scott";
+    cout << "Hi, my name is " << name << ".  What is yours?" << endl << endl;
+    string name_2;
+    cout << "Please enter your name: " << endl << endl;
+    cin >> name_2;
+    cout << endl << endl;
+    cout << "Please to meet you " << name_2 << "...";
+    cout << endl << endl;
+
+
+    return 0;
+}
